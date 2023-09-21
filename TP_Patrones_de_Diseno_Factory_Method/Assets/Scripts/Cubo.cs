18002d761e97afc5462a0ecb1f77a65ad00f4e6e
Luca Moyano <luca-moyano@hotmail.com>
2023-09-20 21:01:33 -0300

    Se crearon prefabs y vincularon scripts

--- TP_Patrones_de_Diseno_Factory_Method/Assets/Scripts/Cubo.cs
+++ TP_Patrones_de_Diseno_Factory_Method/Assets/Scripts/Cubo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cubos : MonoBehaviour
+public class Cubo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
