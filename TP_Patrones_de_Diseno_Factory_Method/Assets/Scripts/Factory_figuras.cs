9f5bd9b6becf8b45d7311f8487a283100bb47b29
Luca Moyano <luca-moyano@hotmail.com>
2023-09-20 21:09:16 -0300

    Se creo script  spawn_giruas

---
+++ TP_Patrones_de_Diseno_Factory_Method/Assets/Scripts/Factory_figuras.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory_figuras : MonoBehaviour
+{
+    [SerializeField] private Figuras figura; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
