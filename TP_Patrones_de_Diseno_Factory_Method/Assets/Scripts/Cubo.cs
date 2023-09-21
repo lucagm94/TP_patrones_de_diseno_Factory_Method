38bbe54e13ec1f93996c55ec1c3831ba82b5bd6c
Luca Moyano <luca-moyano@hotmail.com>
2023-09-20 21:05:09 -0300

    Se creo el script figuras, ahora esfera y cubo hereda de figuras

--- TP_Patrones_de_Diseno_Factory_Method/Assets/Scripts/Cubo.cs
+++ TP_Patrones_de_Diseno_Factory_Method/Assets/Scripts/Cubo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cubo : MonoBehaviour
+public class Cubo : Figuras
 {
     // Start is called before the first frame update
     void Start()
--- TP_Patrones_de_Diseno_Factory_Method/Assets/Scripts/Figuras.cs
+++ TP_Patrones_de_Diseno_Factory_Method/Assets/Scripts/Figuras.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Esfera : Figuras
+public class Figuras : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
